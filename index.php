36ae8db673392add5dbcd6e49556a46a8eee5823
Desi Siti Maryam <desisitimaryam@gmail.com>
2020-03-03 17:10:45 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non consectetur maiores voluptates debitis quae ab sunt vero pariatur totam, cumque dolore. Expedita hic quidem aut libero sequi nam corporis perspiciatis?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
